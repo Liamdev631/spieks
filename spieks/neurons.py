5f1f1fbed12a701dcfe6c7af2e9e84931f1fe781
Liam Bury <liambury@outlook.com>
2025-01-08 20:15:50 +0800

    Removed buffer registration from neuron models

--- spieks/neurons.py
+++ spieks/neurons.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class StatelikeModule(nn.Module):
     def __init__(self, dt=1e-3):
         super().__init__()
-        self.register_buffer('dt', torch.tensor(dt), persistent=False)
+        self.dt = torch.tensor(dt)
         self.initialized = False
     
     def setup(self, x):
@@ -24,16 +24,14 @@ class LearningRule(StatelikeModule):
 class SpikingNeuron(StatelikeModule):
     def __init__(self, dt=1e-3, v_r=0.0, v_th=1.0):
         super().__init__(dt)
-        self.register_buffer('v_r', torch.tensor(v_r), persistent=False)
-        self.register_buffer('v_th', torch.tensor(v_th), persistent=False)
+        self.v_r = torch.tensor(v_r)
+        self.v_th = torch.tensor(v_th)
         self.learning_rules: list[LearningRule] = []
 
     def setup(self, x):
         super().setup(x)
-        self.register_buffer('v', torch.zeros_like(x), persistent=False)
-        self.register_buffer('spikes', torch.zeros_like(x, dtype=torch.bool), persistent=False)
-        for lr in self.learning_rules:
-            self.add_module("learning_rule", lr)
+        self.v = torch.zeros_like(x)
+        self.spikes = torch.zeros_like(x, dtype=torch.bool)
 
     def forward(self, x):
         super().forward(x)
@@ -51,8 +49,7 @@ class IF(SpikingNeuron):
         self.v += x * self.dt
         self.spikes = self.v > self.v_th
         self.v[self.spikes] -= (self.v_th - self.v_r) # Subtractive reset, more accurate
-        return self.spikes.float() * self.rescale_factor
-        #return self.spikes.float()
+        return self.spikes.float() * (self.v_th - self.v_r) * self.rescale_factor
 
     def extra_repr(self):
-        return f"log(dt)={torch.log10(self.dt):.1f}, v_r={self.v_r}, v_th={self.v_th}"
+        return f"log(dt)={torch.log10(self.dt):.0f}, v_r={self.v_r}, v_th={self.v_th}"
