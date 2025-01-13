6990d5d6411e6015abdf39ece14bc90ed6f07671
Liam Bury <liambury@outlook.com>
2025-01-13 13:26:01 +0800

    Added default value to neuron_args in swap_layers

--- spieks/network/converter.py
+++ spieks/network/converter.py
@@ -24,7 +24,7 @@ class Converter():
 
         return SpikingNetwork(new_net, dt)
 
-def swap_layers(model: nn.Module, old_layer_type: type[nn.Module], new_layer_type: type[nn.Module], neuron_args):
+def swap_layers(model: nn.Module, old_layer_type: type[nn.Module], new_layer_type: type[nn.Module], neuron_args={}):
     for name, module in model.named_children():
         if isinstance(module, old_layer_type):
             setattr(model, name, new_layer_type(*module.parameters(), **neuron_args))
